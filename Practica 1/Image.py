9c65d9905b7aaec2fede536dbd351680d7cd39f4
AllGamers <alex_es_sa@hotmail.com>
2021-10-17 02:55:21 -0500

    Commit

--- Practica 1/Image.py
+++ Practica 1/Image.py
@@ -1,9 +1,20 @@
 from PIL import Image
 import numpy as np
 
-w, h = 10, 10
-data = np.zeros((h, w, 3), dtype=np.uint8)
-data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
-img = Image.fromarray(data, 'RGB')
-img.save('my.png')
-img.show()
+def a():
+    w, h = 750, 750 
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
+    img = Image.fromarray(data, 'RGB')
+    img.save('my.png')
+    img.show()
+
+def readFile(fileName):
+    fileObj = open(fileName, "r") #opens the file in read mode
+    words = fileObj.read().splitlines() #puts the file into an array
+    fileObj.close()
+    return words
+
+words = readFile("lab1.txt")
+for word in words:
+    print (word)
