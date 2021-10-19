258870b76880e290fd8751fa7e225e41b019717e
XtreMeiGravity <xtremeigraviti@hotmail.com>
2021-10-19 00:14:54 -0500

    BB

--- Practica 1/Image.py
+++ Practica 1/Image.py
@@ -6,11 +6,12 @@ import enum
 
 
 class Terrain(enum.Enum):
-    Mountain = 0    #128,128,128
-    Land = 1        #250,191,143
-    Water = 2       #0,175,255
-    Sand = 3        #255,192,0
-    Forest = 4      #150,210,80
+    Mountain = 0  # 128,128,128
+    Land = 1  # 250,191,143
+    Water = 2  # 0,175,255
+    Sand = 3  # 255,192,0
+    Forest = 4  # 150,210,80
+
 
 class Stage:
     # memory of cells (Tree)
@@ -21,9 +22,15 @@ class Stage:
     pointActual = None
     # pont final
     pointFinal = None
+    # cells hide
+    cellsHide = []
 
     def __init__(self, stage):
         self.stage = stage
+
+    def addCellsHide(self, number, letter):
+        if not self.cellsHide.__contains__((number, letter)):
+            self.cellsHide.append((number, letter))
 
     def setValuesStage(self, initPoint, finalPoint):
         self.init = initPoint
@@ -44,21 +51,21 @@ class Stage:
 
     def escenarioToImage(self, colors, path):
         w, h = 750, 750
-        wf, hf = w/len(self.stage),h/len(self.stage)
+        wf, hf = w / len(self.stage), h / len(self.stage)
         data = np.zeros((h, w, 3), dtype=np.uint8)
-        for countx,frameX in enumerate(self.stage):
-            for county,frameY in enumerate(frameX):
-                for countc,color in enumerate(colors):
-                    if int(frameY)==countc:
-                        data[countx*int(wf):(countx+1)*int(wf), county*int(hf):(county+1)*int(hf)] = color
-                        #cuadrado
+        for countx, frameX in enumerate(self.stage):
+            for county, frameY in enumerate(frameX):
+                for countc, color in enumerate(colors):
+                    if int(frameY) == countc:
+                        data[countx * int(wf):(countx + 1) * int(wf), county * int(hf):(county + 1) * int(hf)] = color
+                        # cuadrado
                         if countx > 0 or countx < 750 and county > 0 or county < 750:
-                            data[countx*int(wf):(countx+1)*int(wf), county*int(hf)] = [0,0,0] #izquierda
-                            data[countx*int(wf), county*int(hf):(county+1)*int(hf)] = [0,0,0] #abajo
+                            data[countx * int(wf):(countx + 1) * int(wf), county * int(hf)] = [0, 0, 0]  # izquierda
+                            data[countx * int(wf), county * int(hf):(county + 1) * int(hf)] = [0, 0, 0]  # abajo
                         break
-                  
+
         img = Image.fromarray(data, 'RGB')
-        img.save(path+'.png')
+        img.save(path + '.png')
         img.show()
 
 
@@ -77,19 +84,25 @@ stage1 = textToEscenario(readFile("lab1.txt"))
 stage1.setValuesStage(initPoint=(7, 'B'), finalPoint=(2, 'B'))
 stage1.printStage()
 print(stage1.cellInfo(1, 'A'))
-stage1.changeTerrain(1, 'A', Terrain.Sand)
 stage1.printStage()
 print(stage1.cellInfo(2, 'B'))
 stage1.escenarioToImage([
-    [128,128,128], 
-    [255,255,255]
-],'lab1')
+    [128, 128, 128],
+    [255, 255, 255]
+], 'lab1')
 
 stage2 = textToEscenario(readFile("lab2.txt"))
 stage2.escenarioToImage([
-    [128,128,128],
-    [250,191,143],
-    [0,175,255],
-    [255,192,0],
-    [150,210,80]
-],'lab2')
+    [128, 128, 128],
+    [250, 191, 143],
+    [0, 175, 255],
+    [255, 192, 0],
+    [150, 210, 80]
+], 'lab2')
+
+stage2.addCellsHide(1,'A')
+print(stage2.cellsHide)
+stage2.addCellsHide(1,'A')
+print(stage2.cellsHide)
+stage2.addCellsHide(1,'B')
+print(stage2.cellsHide)
